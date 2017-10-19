89195ffc6cda05abeb2f1308bf9cb29b6752af30
Administrator <1486116063@qq.com>
2017-10-19 19:10:36 +0800

    拷贝修改第一次上传

--- app/src/main/java/com/mei/day01_mvp2/MainActivity.java
+++ app/src/main/java/com/mei/day01_mvp2/MainActivity.java
@@ -13,7 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
-        initDate();
+        initText();
+        initText();
+        initText();
+
+    }
+
+    private void initText() {
 
     }
 
