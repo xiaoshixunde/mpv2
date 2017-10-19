1b7639fa8b250c8fd0e3958871ea99e50f3616fc
Administrator <1486116063@qq.com>
2017-10-19 19:06:05 +0800

    vmp2的第一次测试上传

---
+++ app/src/main/java/com/mei/day01_mvp2/MainActivity.java
@@ -0,0 +1,27 @@
+package com.mei.day01_mvp2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView text2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        initDate();
+
+    }
+
+    private void initDate() {
+
+    }
+
+    private void initView() {
+        text2 = (TextView) findViewById(R.id.text2);
+    }
+}
